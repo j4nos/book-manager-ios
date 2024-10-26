373bc981c8649f86a33b0beb1e4ebdc75eb77ef2
János Kukoda <kukodajanos@icloud.com>
2024-10-26 22:26:38 +0200

    Initial Commit

---
+++ BookManagerSwiftUI/BookManagerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookManagerSwiftUIApp.swift
+//  BookManagerSwiftUI
+//
+//  Created by János Kukoda on 2024. 10. 26..
+//
+
+import SwiftUI
+
+@main
+struct BookManagerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookManagerSwiftUITests/BookManagerSwiftUITests.swift
@@ -0,0 +1,16 @@
+//
+//  BookManagerSwiftUITests.swift
+//  BookManagerSwiftUITests
+//
+//  Created by János Kukoda on 2024. 10. 26..
+//
+
+import Testing
+
+struct BookManagerSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
